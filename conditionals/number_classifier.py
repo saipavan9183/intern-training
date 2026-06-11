cbc0e19513f5e7a81c62f5807796d1cb40a0c5c9
saipavan9183 <saipavan9183@gmail.com>
2026-06-11 09:48:42 +0530

    day-3-conditional-statements (#1)

---
+++ conditionals/number_classifier.py
@@ -0,0 +1,16 @@
+# number classifier
+
+number = int(input("enter the number : "))
+
+if number>0 :
+    print("positive")
+elif number<0:
+    print("negative")
+else:
+    print("zero")
+
+if number%2==0:
+    print("even")
+else:
+    print("odd")
+
