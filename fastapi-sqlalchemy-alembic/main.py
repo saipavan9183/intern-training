ada1700c3b87094d8380e9f99080040b0e2c3e86
sai pavan <saipavan9183@gmail.com>
2026-07-14 12:19:50 +0530

    Remove old tanstack-react-app

--- fastapi-sqlalchemy-alembic/main.py
+++ fastapi-sqlalchemy-alembic/main.py
@@ -3,10 +3,20 @@ from fastapi import FastAPI
 from task_routes import router
 from database import engine
 from models import Base
+from fastapi.middleware.cors import CORSMiddleware
+
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
